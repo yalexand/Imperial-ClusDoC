b1288b2bee60f150c9953b49c9a0ba07b1ef6e82
THECRICK\alexany <yalexand@ic.ac.uk>
2020-07-09 09:46:49 +0100

    improved and renamed "Analyze_Colocalisation_per_FOV"

--- Classes/@ClusDoC_analysis_controller/Analyze_Colocalisation_per_FOV.m
+++ Classes/@ClusDoC_analysis_controller/Analyze_Colocalisation_per_FOV.m
@@ -1,4 +1,4 @@
-function [v,info] = Analyze_Colocalisation_per_image(obj,~)
+function [v,info] = Analyze_Colocalisation_per_FOV(obj,~)
 
     pixel_size_in_microns = 0.05; % 50 nanometers
     
@@ -15,15 +15,29 @@ function [v,info] = Analyze_Colocalisation_per_image(obj,~)
         % J Neurosci. 2004 Apr 21;24(16):4070-81. 
         % A Syntaxin 1, Galpha(o), and N-type Calcium Channel Complex at a Presynaptic Nerve Terminal: Analysis by Quantitative Immunocolocalization
         % Qi Li 1, Anthony Lau, et al   
-    mask = n1>0 & n2>0;
+     mask = n1>0 & n2>0;
+    
     corrimg = n1.*n2.*mask;
     s = corrimg(mask);
-    ICA_score = mean(log(s(:)));
+    %ICA_score = mean(log(s));
+    ICA_score = mean(log(s))/std(log(s)); % this works as well
+    %ICA_score = mean(s)/std(s);          % this doesn't work (?) 
+    corrimg = log(1+corrimg);
+
+    % this gives nice ICA image
+%     s1 = n1(mask);
+%     s2 = n2(mask);
+%     corrimg = (n1-mean(s1)).*(n2-mean(s2))/std(s1)/std(s2);
+%     corrimg(mask==0)=0;
+%     % use multiplicative way on corrimg>0 to get co-localization quantifier  
+%     s = n1.*n2.*(corrimg>0);
+%     s = s(s~=0);
+%     ICA_score = mean(log(s))/std(log(s));  
     %
     v = zeros(size(n1,1),size(n1,2),3,1,1);
     v(:,:,1,1,1) = n1;
     v(:,:,2,1,1) = n2;
-    v(:,:,3,1,1) = log(1+corrimg);
+    v(:,:,3,1,1) = corrimg;
         
     max_size = 49; % 7x7 pixels ~ 350x350 nm;
     n1_bigs_mask = bwareaopen(n1>0,max_size);
